9048564258a231a865703915c0096e1186190eaa
Tom Kerkhove <kerkhove.tom@gmail.com>
2020-01-11 15:45:07 +0100

    Serverless bacon
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>

---
+++ src/Bacon/Bacon.Serverless/GetBaconFunction.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace Bacon.Serverless
+{
+    public static class GetBaconFunction
+    {
+        private static readonly List<string> BaconFlavors = new List<string>
+        {
+            "Slab Bacon",
+            "Canadian Bacon",
+            "Smoked Bacon"
+        };
+
+        [FunctionName("bacon-get")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "api/v1/bacon")] HttpRequest req,
+            ILogger log)
+        {
+            return new OkObjectResult(BaconFlavors);
+        }
+    }
+}
